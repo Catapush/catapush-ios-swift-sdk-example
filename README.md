cc7eef6084efca096c746d5a31ed935a250db51d
Alessandro Chiarotto <chiarotto.alessandro@gmail.com>
2015-12-10 11:25:45 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# swift-catapush-ios-sdk-example
+# catapush-ios-swift-sdk-example
 Catapush sdk iOS App Example written in Swift
---
+++ catapush-ios-swift-sdk-example-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  catapush-ios-swift-sdk-example-Bridging-Header.h.h
+//  catapush-ios-swift-sdk-example
+//
+//  Created by Chiarotto Alessandro on 10/12/15.
+//  Copyright © 2015 Catapush s.r.l. All rights reserved.
+//
+
+#ifndef catapush_ios_swift_sdk_example_Bridging_Header_h_h
+#define catapush_ios_swift_sdk_example_Bridging_Header_h_h
+
+#import "CatapushHeaders.h"
+#endif /* catapush_ios_swift_sdk_example_Bridging_Header_h_h */
