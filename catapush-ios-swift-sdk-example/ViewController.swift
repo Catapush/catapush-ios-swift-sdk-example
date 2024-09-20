43b9bdf2b1d978e4d1e83072163b38eb3984fa6c
SnehaMagtarpara <126645402+SnehaMagtarpara@users.noreply.github.com>
2024-09-20 17:20:45 +0530

    Update ViewController.swift

--- catapush-ios-swift-sdk-example/ViewController.swift
+++ catapush-ios-swift-sdk-example/ViewController.swift
@@ -72,19 +72,6 @@ class ViewController: UIViewController,
     
     }
     
-    /*
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: IndexPath) -> UICollectionViewCell {
-        let messageCell:MessageCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(
-            Constants.View.MessageCollectionViewCellId,
-            forIndexPath: indexPath) as! MessageCollectionViewCell
-        let messageIP = self.fetchedResultsController.objectAtIndexPath(indexPath) as! MessageIP
-        messageCell.messageTextView.text = messageIP.body
-        if let previousDate = self.previousDate(messageIP, indexPath: indexPath) {
-            messageCell.setTimestamp(previousDate)
-        }
-        return messageCell
-    }
-    */
     func collectionView(_ collectionView: UICollectionView,layout collectionViewLayout: UICollectionViewLayout,sizeForItemAt indexPath: IndexPath) -> CGSize {
     
 
@@ -94,18 +81,6 @@ class ViewController: UIViewController,
         let cellSize = MessageCollectionViewCell.sizeThatFits(size,text:messageIP.body,showTimestamp:showTimestamp)
         return CGSize(width:self.collectionView.frame.size.width,height: cellSize.height)
     }
-    
-    /*
-    private func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let messageIP = self.fetchedResultsController.objectAtIndexPath(indexPath) as! MessageIP
-        let size = CGSize(width:collectionView.frame.size.width,height:CGFloat.max)
-        let showTimestamp:Bool = (self.previousDate(messageIP, indexPath: indexPath) != nil)
-        let cellSize = MessageCollectionViewCell.sizeThatFits(size,text:messageIP.body,showTimestamp:showTimestamp)
-        return CGSizeMake(self.collectionView.frame.size.width, cellSize.height)
-    }*/
-    
-    
-        
     
     // NSFetchedResultsControllerDelegate
     
@@ -138,17 +113,12 @@ class ViewController: UIViewController,
             self.collectionView.reloadData()
         }
     }
-  
-    /**
-        Return previous date of message is 5 min
-    
-    */
+   
     func previousDate(_ message:MessageIP,indexPath:IndexPath) -> Date! {
         if (indexPath.item > 0) {
             let prevIndex = IndexPath(item: indexPath.row - 1, section: indexPath.section)
             let prevMsg:MessageIP = self.fetchedResultsController.object(at: prevIndex) as! MessageIP
             let timeGap = message.sentTime.timeIntervalSince(prevMsg.sentTime)
-            // let timeGap:TimeInterval = message.sentTime.timeIntervalSinceDate(prevMsg.sentTime)
             if timeGap/60 > 5 {
                 return message.sentTime;
             } else {
